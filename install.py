2a1cfb296d785dd0bc128f22bb1b3c001b787b35
Egor Hamaliy <egor.hamaliy@gmail.com>
2016-02-26 19:46:50 +0300

    Python install script added.

--- install.py
+++ install.py
@@ -1,18 +1,39 @@
 import os
 import urllib
+from subprocess import call
 
 font_path = "{0}/.fonts".format(os.getenv("HOME"))
-font_list = ["Monaco_Linux.ttf", "aaaa", "dgsdfg"]
+font_list = ["Monaco-Linux.ttf", "Menlo-Regular.ttf"]
+url = "https://github.com/egorhm/linux-fonts/raw/master/fonts"
 
 
-def download_fonts(filename):
-    filename = urllib.urlretrieve("https://github.com/egorhm/dotfiles/blob/master/gitconfig", "testfile.txt")
-    print(filename)
+def download_font(filename):
+    dwn_url = "{0}/{1}".format(url, filename)
+    local_path = "{0}/{1}".format(font_path, filename)
+    localname = urllib.urlretrieve(dwn_url, local_path)
+    return localname
+
+
+def install():
+    if not os.path.exists(font_path):
+        print("{0} doesn't exist and will be created".format(font_path))
+        os.makedirs(font_path)
+        print("dir created")
+
+    for filename in font_list:
+        font_file = download_font(filename)
+        print("{0} downloaded to {1}".format(filename, font_file))
+
+    print("calling fc-cache...")
+    call(["fc-cache"])
 
 
 if __name__ == '__main__':
     print("--------------------------------------------------------------")
     print("Installing fonts")
+    print("github: https://github.com/egorhm/linux-fonts")
+    print("--------------------------------------------------------------\n")
+    install()
+    print("\n--------------------------------------------------------------")
+    print("Fonts installed")
     print("--------------------------------------------------------------")
-
-    download_fonts()
